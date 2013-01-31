243ed7cd574456b2b1d8e535cd251a2b6456ddb9
Nicholas Kamper <kampernj@rose-hulman.edu>
2013-01-31 18:25:48 -0500

    Added spiralIterator and a new .gitignore to ignore MATLAB temporary files
    Signed-off-by: Nicholas Kamper <kampernj@rose-hulman.edu>

---
+++ Code/spiralIterator.m
@@ -0,0 +1,31 @@
+function spiralIterator(mtx, start, func)
+    r = start(1);
+    c = start(2);
+    R = size(mtx, 1);
+    C = size(mtx, 2); 
+    
+    dx = 0;
+    dy = -1;
+    
+    x = 0;
+    y = 0;
+    for i=1:max(2*R, 2*C)^2
+        if (r + x > 0 && r + x <= R) && (c + y > 0 && c + y <= C)
+            % Valid point!
+            if (func(mtx, r + x, c + y) == -1)
+                break;
+            end
+        end
+        
+        % Check if we should swap direction
+        if (x == y) || (x < 0 && x == -y) || (x > 0 && x == 1 - y)
+            t = -dy;
+            dy = dx;
+            dx = t;
+        end
+        
+        % Update the new coordinates
+        x = x + dx;
+        y = y + dy; 
+    end
+end

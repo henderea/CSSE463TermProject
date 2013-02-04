e2a25456fafa7f25a6fc34cfe83ea28c689c0585
James Savage <axiixc@gmail.com>
2013-02-04 09:11:51 -0500

    Updated status report

--- VarianceDirectionFinding/main.m
+++ VarianceDirectionFinding/main.m
@@ -1,16 +1,16 @@
 function main
     im = getCat();
     imtool(mdv(im, 20));
-    [r,c] = mdvpoi(im, 20)
-    boxes = mdvboi(im, 20);
+    [r,c] = mdvpoi(im, 15)
+    boxes = mdvboi(im, 15);
     size(boxes)
     
     imshow(im);
     hold on;
-    plot(c, r, 'b*');
     for i = 1:size(boxes,1)
         rectangle('position', boxes(i,:), 'EdgeColor', 'r', 'FaceColor', 'r');
     end
+    plot(c, r, 'b*');
     hold off;
 end
 

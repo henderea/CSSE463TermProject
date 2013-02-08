0b7ab9efe88c2bee710085a0675f3883cef9e3ab
Nicholas Kamper <kampernj@rose-hulman.edu>
2013-02-07 21:49:40 -0500

    stuff:
    Signed-off-by: Nicholas Kamper <kampernj@rose-hulman.edu>

--- Code/runGaussianPointMatcher.m
+++ Code/runGaussianPointMatcher.m
@@ -1,5 +1,5 @@
 function [r,c] = runGaussianPointMatcher(A, B, POI, windowSize, sigma, threshold)
-    fn = getGaussianCorrespondenceFunction(A, B, POI, sigma, windowSize, windowSize);
-    wrappedFn = getThresholdedIteratorAdapter(fn, threshold);
+    fn = getGaussianCorrespondenceFunction(A, B, POI, sigma, windowSize, windowSize, threshold);
+    wrappedFn = getThresholdedIteratorAdapter(fn, intmax - 1);
     [r,c] = spiralIterator(B, POI, wrappedFn);
 end

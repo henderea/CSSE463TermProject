0eb2bce63d6307af8d683f64390cc2fcef236015
Nicholas Kamper <kampernj@rose-hulman.edu>
2013-02-11 19:50:44 -0500

    this code is broken. I don't know why.
    Signed-off-by: Nicholas Kamper <kampernj@rose-hulman.edu>

--- ImprovedCodeV2.0/findMatches.m
+++ ImprovedCodeV2.0/findMatches.m
@@ -2,7 +2,8 @@ function [matches] = findMatches(A, B)
     % CONFIGURATION
     winSize = [5 5];
     maxCorners = 1000;
-    threshold = 10;
+    threshold = 200;
+    bandSize = 10; % px
     
     % The corner detector only works in grayscale space
     gA = rgb2gray(A);
@@ -11,8 +12,8 @@ function [matches] = findMatches(A, B)
     % Find corners in A and B
     cornerDetector = vision.CornerDetector;
     cornerDetector.MaximumCornerCount = maxCorners;
-    cA = step(cornerDetector, gA)
-    cB = step(cornerDetector, gB)
+    cA = step(cornerDetector, gA);
+    cB = step(cornerDetector, gB);
     
     % Indices already correlated 
     iA = zeros(size(cA, 1), 1);
@@ -21,14 +22,18 @@ function [matches] = findMatches(A, B)
     for i=1:size(cA, 1)
         if ~iA(i)
             winA = getWindow(gA, cA(i, :), winSize);
-            % Check if this corner exists in B
-            for j=1:size(cB, 1)
-                if ~iB(j)
-                    % Are these corresponding regions?
-                    winB = getWindow(gB, cB(j, :), winSize);
+            
+            % Get a list of the candidate matches from cB
+            match1 = (cB(:, 1) > (cA(i, 1) - bandSize));
+            match2 = (cB(:, 1) < (cA(i, 1) + bandSize));
+            bMatches = find((match1 & match2) == 1);
+
+            for j=1:size(bMatches)
+                if ~iB(bMatches(j))
+                    winB = getWindow(gB, cB(bMatches(j), :), winSize);
                     if getMatchScore(winA, winB, ones(size(winA))) < threshold
-                        iB(j) = i;
-                        iA(i) = j;
+                        iB(i) = bMatches(j);
+                        iA(bMatches(j)) = i;
                         break;
                     end
                 end
@@ -39,14 +44,17 @@ function [matches] = findMatches(A, B)
     for i=1:size(cB, 1)
         if ~iB(i)
             winB = getWindow(gB, cB(i, :), winSize);
-            % Check if this corner exists in A
-            for j=1:size(cA, 1)
-                if ~iA(j)
-                    % Are these corresponding regions?
-                    winA = getWindow(gA, cA(j, :), winSize);
+            % Get a list of the candidate matches from cA
+            match1 = (cA(:, 1) > (cB(i, 1) - bandSize));
+            match2 = (cA(:, 1) < (cB(i, 1) + bandSize));
+            aMatches = find((match1 & match2) == 1);
+            
+            for j=1:size(aMatches)
+                if ~iA(aMatches(j))
+                    winA = getWindow(gA, cA(aMatches(j), :), winSize);
                     if getMatchScore(winA, winB, ones(size(winA))) < threshold
-                        iB(i) = j;
-                        iA(j) = i;
+                        iA(i) = aMatches(j);
+                        iB(aMatches(j)) = i;
                         break;
                     end
                 end
